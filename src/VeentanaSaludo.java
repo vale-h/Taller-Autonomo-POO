398fb3572f72e11dee89ecdce7b741cf30de5ebb
HP Virtus <v.hormazabal03@ufromail.cl>
2025-09-04 22:02:46 -0400

    Codigo de Actividad 1

---
+++ src/VeentanaSaludo.java
@@ -0,0 +1,30 @@
+import javax.swing.*;
+import java.awt.event.*;
+
+public class VeentanaSaludo {
+    public static void main(String[] args) {
+        JFrame ventana = new JFrame("App de Saludo ICC490");
+        ventana.setSize(400,200);
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ventana.setLayout(null);
+
+        JTextField campoTexto = new JTextField();
+        campoTexto.setBounds(50,30,200,25);
+
+        JButton botonSaludar = new JButton("Saludar");
+        botonSaludar.setBounds(270,30,100,25);
+
+        JLabel etiquetaSaludo = new JLabel("");
+        etiquetaSaludo.setBounds(50,80,300,25);
+
+        botonSaludar.addActionListener(e -> {
+            String nombre = campoTexto.getText();
+            etiquetaSaludo.setText("Hola " + nombre);
+        });
+        ventana.add(botonSaludar);
+        ventana.add(etiquetaSaludo);
+        ventana.add(campoTexto);
+        ventana.setLocationRelativeTo(null);
+        ventana.setVisible(true);
+    }
+}
